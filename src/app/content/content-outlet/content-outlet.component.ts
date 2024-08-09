e41e97599c610693a228de19b94c7849c0c5ac6f
Henru Olivier <henrupetrusolivier@gmail.com>
2024-08-09 11:35:16 +0200

    cleanup and transformation

--- src/app/content/content-outlet/content-outlet.component.ts
+++ src/app/content/content-outlet/content-outlet.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { IntroPageComponent } from '../intro-page/intro-page.component';
-import { DevExperienceComponent } from '../dev-experience/dev-experience.component';
-import { EducationComponent } from '../education/education.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
 import { ProjectsComponent } from '../projects/projects.component';
 
 @Component({
   selector: 'app-content-outlet',
   standalone: true,
-  imports: [IntroPageComponent, DevExperienceComponent, EducationComponent, TechnologiesComponent, ProjectsComponent],
+  imports: [IntroPageComponent, TechnologiesComponent, ProjectsComponent],
   templateUrl: './content-outlet.component.html',
   styleUrl: './content-outlet.component.scss'
 })
