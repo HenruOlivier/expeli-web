3a900fa71ec37e00f427c94fafde99683d4c22a0
Henru Olivier <henrupetrusolivier@gmail.com>
2024-08-09 12:32:50 +0200

    introduction section update

--- src/app/content/content-outlet/content-outlet.component.ts
+++ src/app/content/content-outlet/content-outlet.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { IntroPageComponent } from '../intro-page/intro-page.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
 import { ProjectsComponent } from '../projects/projects.component';
+import { IntroductionComponent } from '../introduction/introduction.component';
 
 @Component({
   selector: 'app-content-outlet',
   standalone: true,
-  imports: [IntroPageComponent, TechnologiesComponent, ProjectsComponent],
+  imports: [IntroPageComponent, IntroductionComponent, TechnologiesComponent, ProjectsComponent],
   templateUrl: './content-outlet.component.html',
   styleUrl: './content-outlet.component.scss'
 })
--- src/app/content/introduction/introduction.component.ts
+++ src/app/content/introduction/introduction.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ToggleInViewDirective } from '../../shared/directives/toggleInViewClass';
+import { TypingEffectDirective } from '../../shared/directives/typingEffect';
 
 @Component({
   selector: 'app-introduction',
   standalone: true,
-  imports: [],
+  imports: [TypingEffectDirective, ToggleInViewDirective],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.scss'
 })
