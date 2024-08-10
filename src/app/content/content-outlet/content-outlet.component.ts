fdd71aac4e6455ea1fea151d32663cc97b9bb586
Henru Olivier <henrupetrusolivier@gmail.com>
2024-08-10 13:27:03 +0200

    process map and what we need component

--- src/app/content/content-outlet/content-outlet.component.ts
+++ src/app/content/content-outlet/content-outlet.component.ts
@@ -3,11 +3,12 @@ import { IntroPageComponent } from '../intro-page/intro-page.component';
 import { TechnologiesComponent } from '../technologies/technologies.component';
 import { IntroductionComponent } from '../introduction/introduction.component';
 import { ServicesComponent } from '../services/services.component';
+import { ProcessMapComponent } from '../process-map/process-map.component';
 
 @Component({
   selector: 'app-content-outlet',
   standalone: true,
-  imports: [IntroPageComponent, IntroductionComponent, TechnologiesComponent, ServicesComponent],
+  imports: [IntroPageComponent, IntroductionComponent, TechnologiesComponent, ServicesComponent, ProcessMapComponent],
   templateUrl: './content-outlet.component.html',
   styleUrl: './content-outlet.component.scss'
 })
---
+++ src/app/content/process-map/process-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './process-map.component.html',
+  styleUrl: './process-map.component.scss'
+})
+export class ProcessMapComponent {
+
+}
